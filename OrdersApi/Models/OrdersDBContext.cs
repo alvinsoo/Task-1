3f71497fe809dcfc7f382b89f4b3eeb78d502b1a
alvinsoo <34090577+alvinsoo@users.noreply.github.com>
2022-02-28 14:19:01 +0800

    Add files via upload

---
+++ OrdersApi/Models/OrdersDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrdersApi.Models
+{
+    public class OrdersDBContext : DbContext
+    {
+        public OrdersDBContext(DbContextOptions<OrdersDBContext> options)
+          : base(options)
+        {
+        }
+
+        public DbSet<Order> TodoItems { get; set; } = null!;
+    }
+}
